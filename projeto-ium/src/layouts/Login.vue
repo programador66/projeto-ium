21f0ffca8ef7d2d662a1fa74297976691cbbcde7
Caio Cesar <sys13@grupopmz.com.br>
2019-09-12 16:08:11 -0400

    login projeto quasar ium

---
+++ projeto-ium/src/layouts/Login.vue
@@ -0,0 +1,99 @@
+<template>
+  <q-layout id="login">
+     <div class="q-pa-md" style="max-width: 400px">
+        <div id="foto">
+             <q-img
+              :src="url"
+              spinner-color="white"
+              style="height: 140px; max-width: 150px"
+              />
+        </div>
+       
+      <q-form
+        class="q-gutter-md"
+        color="white"
+      >
+        <q-input
+          color="white"
+          filled
+          v-model="usuario"
+          label="Usuário"
+          lazy-rules
+          dark
+          :rules="[ val => val && val.length > 0 || 'Campo não pode ser nulo']"
+        />
+
+        <q-input
+          filled
+          color="white"
+          v-model="senha"
+          label="Senha"
+           dark
+          lazy-rules
+          type="password"
+          :rules="[
+            val => val !== null && val !== '' || 'Campo não pode ser nullo'
+          ]"
+        />
+        <div id="ntc"> Não tem conta? Crie uma nova conta  </div>
+        <div>
+          <q-btn label="Submit" @click="onSubmit" text-color="blue" color="white"/>
+        </div>
+      </q-form>
+    </div>
+  </q-layout>
+</template>
+
+<script>
+import imagem from '../img/user.png';
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+      usuario: null,
+      senha: null,
+      url: imagem,
+      accept: true
+    }
+  },
+   methods: {
+    onSubmit () {
+      if (this.accept !== true) {
+        this.$q.notify({
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'fas fa-exclamation-triangle',
+          message: 'You need to accept the license and terms first'
+        })
+      }
+      else {
+        this.$q.notify({
+          color: 'green-4',
+          textColor: 'white',
+          icon: 'fas fa-check-circle',
+          message: 'Submitted'
+        })
+      }
+    },
+
+    onReset () {
+      this.usuario = null
+      this.senha = null
+      this.accept = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+#login {
+  background-color:#1E88E5;
+}
+#foto{
+  margin:20% 28%;
+}
+#ntc{
+  color:white;
+}
+</style>
---
+++ projeto-ium/src/pages/Index.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="q-pa-md">
+    <q-card class="my-card">
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-tabs v-model="tab" class="text-teal" >
+        <q-tab label="Tab one" class="text" name="one" />
+        <q-tab label="Tab two" class="text" name="two" />
+         <q-tab label="Tab three" class="text" name="three" />
+      </q-tabs>
+
+      <q-separator />
+
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="one">
+          The QCard component is a great way to display important pieces of grouped content.
+        </q-tab-panel>
+
+        <q-tab-panel name="two">
+          With so much content to display at once, and often so little screen real-estate,
+          Cards have fast become the design pattern of choice for many companies, including
+          the likes of Google and Twitter.
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+  .text{
+    color:blue
+  }
+</style>
+
+<script>
+export default {
+    data () {
+    return {
+       tab: 'one'
+    }
+  }
+}
+</script>
