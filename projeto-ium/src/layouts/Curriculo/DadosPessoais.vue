ffda825bb616fab32b0a1b61b936e960b3d184d4
Caio Lacerda <kaio.cesr@gmail.com>
2019-11-07 17:01:21 -0400

    atualizações

--- projeto-ium/src/layouts/Curriculo/DadosPessoais.vue
+++ projeto-ium/src/layouts/Curriculo/DadosPessoais.vue
@@ -1,19 +1,29 @@
 <template>
-   <div class="q-pa-md">
+   <q-layout view="hHh lpR fFf">
 
     <div class="row">
-      <div class="col">col</div>
-      <div class="col">col</div>
-      <div class="col">col</div>
-      <div class="col">col</div>
+      <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar class="bg-primary text-white">
+      <q-btn flat round dense icon="keyboard_backspace" class="q-mr-sm"  />
+      
+      <q-toolbar-title>Criar Perfil</q-toolbar-title>
+
+      <q-btn flat label="concluir"/>
+    </q-toolbar>
+      
+    </q-header>
     </div>
 
-    <div class="row">
-      <div class="col-8">col-8</div>
-      <div class="col-4">col-4</div>
+  
+    <div id="passos">
+       <q-card class="my-card">
+      <q-card-section>
+        testetstetetetettasdasdasdasd asd asdasd a 
+      </q-card-section>
+    </q-card>
     </div>
 
-  </div>
+  </q-layout>
 </template>
 
 <script>
@@ -33,7 +43,10 @@ export default {
 </script>
 
 <style scoped>
-
+#passos{
+  margin-top: 13%;
+  height: 50px;
+}
 
 @media only screen and (max-width:320px) {
  
