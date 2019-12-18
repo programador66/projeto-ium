f00aa042b97230c093c2170c5922c3169d06fa8b
Caio Lacerda <kaio.cesr@gmail.com>
2019-12-18 16:13:53 -0400

    icone delete

--- projeto-ium/src/layouts/Curriculo/Educacao.vue
+++ projeto-ium/src/layouts/Curriculo/Educacao.vue
@@ -9,10 +9,15 @@
           </q-item-label> 
 
             <template v-if="existeEducacao">
-              <div  v-for="edu in objEducacao" :key="edu">
+              <div  v-for="(edu,indice) in objEducacao" :key="indice">
                 <q-card-section> 
                   <q-item-label>
-                    <span>{{edu.titulo}}</span> <span style="float:right;color:#1E88E5;font-weight:bold;">{{edu.conclusao}}</span>
+                    <span>{{edu.titulo}}</span>
+                     <span style="float:right;color:#1E88E5;font-weight:bold;">
+                      {{edu.conclusao}}
+                      <span ><q-btn flat round icon="delete"/></span>
+                      </span>
+                      
                   </q-item-label>
                   <q-item-label>
                     {{edu.instituicao}}
@@ -48,7 +53,7 @@
               <span style="float:left">
               <q-input v-model="inicio" 
               label="Inicio" 
-              style="max-width:150px"
+              style="max-width:148px"
               mask="##/##/####"
               hint="ex: 00/00/0000"
               ref="dtinicio"
@@ -63,7 +68,7 @@
               <span style="float:right">
               <q-input v-model="conclusao"
               label="Conclusao"
-              style="max-width:150px;"
+              style="max-width:145px;"
               mask="##/##/####"
               hint="ex: 00/00/0000"
               ref="dtconclusao"
@@ -111,7 +116,7 @@ export default {
   name: 'Educacao',
   data () {
     return {
-     objEducacao:[{"titulo":"Sistemas de Informação","instituicao":"FUCAPI","conclusao":2016}], 
+     objEducacao:[], 
      curso:null,
      instituicao:null,
      inicio:null,
@@ -129,7 +134,22 @@ export default {
       this.$refs.instituicao.validate();
       this.$refs.dtinicio.validate();
       this.$refs.dtconclusao.validate();
-      
+
+      if (this.$refs.curso.hasError || this.$refs.instituicao.hasError || this.$refs.dtinicio.hasError || this.$refs.dtconclusao.hasError) {
+        return this.formHasError = true;
+      }
+
+      const educacao = {
+        curso: this.curso,
+        instituicao: this.instituicao,
+        inicio: this.inicio,
+        conclusao: this.conclusao
+      }
+
+       this.existeEducacao = true; 
+       this.objEducacao.push(educacao);
+       console.log(this.objEducacao);
+
     }
   }
 }
